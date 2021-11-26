6514fa4d441648d7e36c0b3f557a53b4332eb788
AngeYP <angelymar080695@gmail.com>
2021-11-26 02:16:13 -0400

    Thread in clientUDP

--- main/main.c
+++ main/main.c
@@ -3,6 +3,6 @@
 
 void main()
 {
-	system("gcc menu.c udp/client.c udp/server.c tcp/client.c tcp/fserver.c -o mcs");
+	system("gcc -g -pthread menu.c udp/client.c udp/server.c tcp/client.c tcp/fserver.c -o mcs");
 	system("./mcs");
 }
--- main/udp/header/cliente.h
+++ main/udp/header/cliente.h
@@ -1,8 +1,10 @@
 #ifndef CLIENT_DOT_H    
 #define CLIENT_DOT_H    
 
+void * connectClient ();
 void cliente_UDP(int);
 void askfind (char *);
+int numberserv();
 void askipadress(char *);
 
 #endif /* CLIENT_DOT_H */
